7efe6d0833e8e48a1be98d8da72f3cbf63d8a24d
Miguel-ES-503 <miguel.barahona633@gmail.com>
2020-07-21 20:10:19 -0600

    Actualización-Coach-Reuniones-4

--- CoachReuniones/DetallesHorasSociales.php
+++ CoachReuniones/DetallesHorasSociales.php
@@ -75,11 +75,11 @@ include 'Modularidad/MenuVertical.php';
 ?>
 <br>
 <!--Comiezo de estructura de trabajo -->
-<div class="container-fluid text-center">
+<!--<div class="container-fluid text-center">
 <div class="title">
     <img src="../img/back.png" class="icon">
   <h2 class="main-title" >Horas de Vinculación</h2>
-</div>
+</div>-->
 <!--/.Navbar-->
 <div class="float-right"> <?php include 'Modularidad/Alerta.php'?></div>
   <!--<div class="row"  >
@@ -88,16 +88,13 @@ include 'Modularidad/MenuVertical.php';
     </div>
   </div>-->
 <div class="text-justify border border-light p-5" id="main">
- 
-
-  <div>
-    <h3 class="mt-0">Detalles del Proyecto</h3>
+    <h3 class="mt-0 mb-3">Detalles del Proyecto</h3>
 
     
 
-    <div class="row mt-4">
-      <div class="col-sm-12  col-md-12  col-lg-6" style="border: solid 1px black;">
-        <div id="Info-1" class="m-lg-5">
+    <div class="row mt-1">
+      <div class="col-sm-12  col-md-12  col-lg-6" id="Contenedor1">
+        <div id="Info-1" class="m-lg-4 m-md-5 m-sm-4 m-3">
           
           <p id="Info-1_Texto-1" ><b>Alumno:</b> </p>
           <p id="Info-1_Texto-1">Miguel Angel Barahona Garcia</p><br>
@@ -113,56 +110,70 @@ include 'Modularidad/MenuVertical.php';
         <hr id="linea">
 
 
-        <div class="container ml-lg-5">
+        <div class="container m-lg-4 m-3">
   <form action="/action_page.php">
     <div class="row">  
-      <div class="col-75">
+      <div class="col-75" id="status">
         <label id="label">Status del Alumno</label>
         <input type="text" id="input" name="firstname">
       </div>
     </div>
     
     <div class="row">
-      <div class="col-75">
+      <div class="col-75" id="status">
         <label id="label">Comentario</label>
         <textarea id="input"  style="height:200px"></textarea>
       </div>
     </div>
-    <div class="row">
-      <input type="submit" value="Submit" id="btn">
+    <br><br>
+<div id="boton1" align="center">
+      <input type="submit" value="Comprobante" id="btn">
     </div>
+      <br><br>
   </form>
 </div>
 
 
       </div>
-      <div class="col-sm-12 mt-sm-4 col-md-12 mt-md-4  col-lg-5 ml-lg-1 mt-lg-0 mt-3" style="border: solid 1px black;">
-        <h1>Hola</h1>
-
+      <div class="col-sm-12 mt-sm-4 col-md-12 mt-md-4  col-lg-5 ml-lg-5 mt-lg-0 mt-3" id="Contenedor2">
+        <div id="Info-2" class="m-lg-3 m-md-4 m-sm-5">
+        <div class="container ml-lg-3">
+  <form action="/action_page.php">
+     <div class="row">  
+      <div class="col-md-12  mr-lg-5">
+          <p id="Info-1_Texto-1" ><b>Estado:</b> </p>
+          <p id="Info-1_Texto-1" style="font-style: italic;">En Espera</p><br>
       </div>
-
     </div>
-    <!--<div class="card-footer text-muted">
-      Solicitud
-    </div>-->
-  </div>
-
-
-
-
-
-  
+    <div class="row">  
+      <div class="col-md-12  mr-lg-5">
+        <label id="label"><b>Comentario</b></label>
+        <textarea id="input"  style="height:200px"></textarea>
+      </div>
+    </div>
+    <br>
+    <br>
+    <br>
+    <div class="row">
+      <div class="col-75 col-md-12">
+        <label id="label"><b>Cambiar Estado</b></label>
+          <select id="input" >
+            <option value=""></option>
+          </select>
+      </div>
+    </div>
+    <br><br><br><br><br>
+    <div id="boton2">
+      <input type="submit" value="Enviar" id="btn">
+    </div>
+      
+      <br><br>
+  </form>
 </div>
 
-<br>
-
-
-
-
-<br>
-<br>
-<br>
-<br>
+      </form>
+    </div>
+  </div>
 <br>
 <?php
 //Incluir el footer
