652a0c73ac017414d1ffb3141987d06bf68f9475
bryanherrera123 <bryan.herrera@oportunidades.org.sv>
2020-07-23 12:17:13 -0600

    Arreglos vista de Pasante

--- Auxiliar/Modularidad/MenuHorizontal.php
+++ Auxiliar/Modularidad/MenuHorizontal.php
@@ -202,69 +202,87 @@
         <ul class="snip1135 ml-lg-3" style="text-align: center;">
         
         
-
-          <li>
+        <li>
             <a class="list-group-item list-group-item-action" href="Calendario.php" >
               <i class="far fa-calendar-alt"></i>  Calendario 
             </a>
+          
           </li>
 
-          
-
           <li>
-            <a class="list-group-item list-group-item-action" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample" style="font-size: 13px;">
-              <i class="fas fa-chalkboard-teacher"></i> Administración <i class="fas fa-sort-down"></i>
+            <a class="list-group-item list-group-item-action" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample" >
+            <i class="fas fa-chalkboard-teacher"></i> Taller <i class="fas fa-sort-down"></i>
             </a>
             <div class="collapse" id="collapseExample">  
               <ul class="mr-lg-1">
-              <li><a href="SIT-CrearReunion.php" class="list-group-item list-group-item-action">Crear Reunión</a></li>
+              <li><a href="SIT-ProcesoInscripcion.php" class="list-group-item list-group-item-action">Hora Inscripción</a></li>
+               <li><a href="SIT-CrearTaller.php" class="list-group-item list-group-item-action">Crear Talleres</a></li>
+               <li><a href="LIS-Talleres.php" class="list-group-item list-group-item-action">Talleres Activos</a></li>
+               <li><a href="ListadoTalleresFinalizado.php" class="list-group-item list-group-item-action">Talleres Finalizados</a></li> 
+               <li><a href="ListadoTalleresDesactivo.php" class="list-group-item list-group-item-action">Talleres Desactivos</a></li>
+             </ul>
+           </div>
+         </li>
+
+         <li>
+            <a class="list-group-item list-group-item-action" data-toggle="collapse" href="#collapseExample4" role="button" aria-expanded="false" aria-controls="collapseExample" style="font-size: 13px;">
+            <i class="fas fa-university"></i> Reuniones<i class="fas fa-sort-down"></i>
+            </a>
+            <div class="collapse" id="collapseExample4">  
+              <ul class="mr-lg-1">
+              <li><a href="SIT-CrearReunion.php" class="list-group-item list-group-item-action">Crear Reunión</a></li>  
               <li><a href="LIS-Reunion.php" class="list-group-item list-group-item-action">Reuniones Activos</a></li>
               <li><a href="reunionesFinalizados.php" class="list-group-item list-group-item-action">Reuniones Finalizadas</a></li>
              </ul>
            </div>
          </li>
-          <li>
-            <a class="list-group-item list-group-item-action" data-toggle="collapse" href="#collapseExample4" role="button" aria-expanded="false" aria-controls="collapseExample">
+
+         <li>
+            <a class="list-group-item list-group-item-action" data-toggle="collapse" href="#collapseExample3" role="button" aria-expanded="false" aria-controls="collapseExample" style="font-size: 13px;">
             <i class="fas fa-user-graduate"></i>Estudiantes<i class="fas fa-sort-down"></i>
             </a>
-            <div class="collapse" id="collapseExample4">  
-              <ul>
-                <li><a href="LIS-Alumnos.php" class="list-group-item list-group-item-action">Alumnos</a></li>
-                <li><a href="RecordAlumnos.php" class="list-group-item list-group-item-action">Récord Alumnos</a></li>
-                <li><a href="BecasAprobadas.php" class="list-group-item list-group-item-action">Estado Beca</a></li> 
-               </ul>
-            </div>
+            <div class="collapse" id="collapseExample3">  
+              <ul class="mr-lg-1">
+              <li><a href="LIS-Alumnos.php" class="list-group-item list-group-item-action">Alumnos</a></li>
+            <li><a href="RecordAlumnos.php" class="list-group-item list-group-item-action">Récord Alumnos</a></li>
+            <li><a href="BecasAprobadas.php" class="list-group-item list-group-item-action">Estado Beca</a></li>
+             </ul>
+           </div>
+         </li>
+
+         <li>
+            <a class="list-group-item list-group-item-action" data-toggle="collapse" href="#collapseExample2" role="button" aria-expanded="false" aria-controls="collapseExample" style="font-size: 13px;">
+            <i class="fas fa-envelope"></i> Solicitud <i class="fas fa-sort-down"></i>
+            </a>
+            <div class="collapse" id="collapseExample2">  
+              <ul class="mr-lg-1">
+              <li><a href="PermisosDesincribirTaller.php" class="list-group-item list-group-item-action">Permisos Talleres</a></li> 
+            <li><a href="HorasVinculacion.php" class="list-group-item list-group-item-action">Horas De Vinculación</a></li> 
+             </ul>
+           </div>
+         </li>
+
+          
+
+          <li>
+          <a class="list-group-item list-group-item-action" href="Graficos.php" >
+          <i class="fas fa-archive"></i> Reportes Talleres 
+        </a>
           </li>
+
+          <li>
+        <a class="list-group-item list-group-item-action" href="ReporteCompetenciasFecha.php" >
+          <i class="fas fa-archive"></i> Reportes Competencias
+        </a>
+      </li>
+
+          
+
+         
+          
 
 <br>
-          <li>
-            <a class="list-group-item list-group-item-action" data-toggle="collapse" href="#collapseExample3" role="button" aria-expanded="false" aria-controls="collapseExample">
-             <i class="far fa-envelope"></i>Solicitud <i class="fas fa-sort-down"></i>
-            </a>
-            <div class="collapse" id="collapseExample3">  
-              <ul>
-              <li><a href="HorasVinculacion.php" class="list-group-item list-group-item-action">Horas De Vinculación</a></li> 
-                  
-               </ul>
-            </div>
-          </li>
-
-             <li>
-        <a class="list-group-item list-group-item-action" href="Manual/ManualTaller.pdf" >
-          <i class="fas fa-info-circle"></i> Instrucciones
-        </a>
-      </li>
-
-      <li>
-        <a class="list-group-item list-group-item-action" href="ReporteReuniones.php" >
-          <i class="fas fa-chart-pie"></i> Reporteria
-        </a>
-      </li>
-      <!-- Link de preguntas Frecuentes -->
-      <li>
-        <a class="list-group-item list-group-item-action" href="preguntas.php" ><i class="fas fa-question-circle"></i> Preguntas Frecuentes
-        </a>
-      </li>
+         
       <!-- fin de preguntas frecuentes -->
       <li>
         <a class="list-group-item list-group-item-action" href="sessionesOneonOne.php"><i class="fas fa-user-friends"></i> Sesiones Individuales
