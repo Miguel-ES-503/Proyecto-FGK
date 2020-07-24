0198db246451dfc3df69f311b2e1ecb429bd0e2d
bryanherrera123 <bryan.herrera@oportunidades.org.sv>
2020-07-24 14:16:29 -0600

    Cambiar datos listo

--- Auxiliar/Modularidad/PiePagina.php
+++ Auxiliar/Modularidad/PiePagina.php
@@ -1,42 +1,25 @@
-</div>          
+</div>
 </div>
 </div>
 <!-- FOOTER START -->
-<div class="footer">
-  <div class="contain">
-    <div class="col">
-      <h1 style="color: white;">Derechos reservados</h1>
-      <ul>
-        <li>
-          <center><img src="../img/logoblanco2.png" width="100" style="width:100px;"></center>
-        </li>
-      </ul>
+<div class="footer-copyright text-center py-3" style="background: black;">
+          <img class="img-fluid" src="../img/funda.png" width="60px">
+          </img>
+          <img class="img-fluid" src="../img/logoblanco2.png" style="margin-left:30px;"></img>
+          <span style="margin-right:50px; margin-left:50px; color:white; font-size: 18px;">© 2020 Copyright: Pograma Oportunidades</span>
+          <span style="color: white; font-weight: bold; font-size: 18px;">Contáctanos:</span><a href="https://www.facebook.com/exalumnos.ccgk"><img class="img-fluid" src="../img/facebook.png" style="margin-left:30px; width:60px;"></img></a>
+          <a href="https://instagram.com/bk2oportunidades?igshid=4rmcd55eld5h"><img class="img-fluid" src="../img/instagram.png" style="margin-left:30px; width:60px;"></a></img>
 
-    </div>
-
-    <div class="col social">
-      <h1 style="color: white;">Contáctanos:</h1>
-      <ul>
-        <li>
-         <center><a href="https://www.facebook.com/exalumnos.ccgk?ref=br_tf&epa=SEARCH_BOX"><img src="../img/facebook.png" width="32" style="width:32px;"></a></center> 
-       </li>
-       <li>
-        <center><a href="https://www.instagram.com/bk2oportunidades/"><img src="../img/instagram.png" width="32" style="width: 32px;"></a></center>
-        
-      </li>
-    </ul>
   </div>
-  <div class="clearfix"></div>
-</div>
-</div>
+
 <!-- END OF FOOTER -->
 
   <!-- Bootstrap core JavaScript -->
-  
+
   <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
   <script src="js/alerta.js"></script> <!--Mostrar el mensaje de alerta-->
 
- 
+
 
 
 
@@ -47,11 +30,11 @@
   <script src="js/FormCarreras.js" ></script>
   <script src="js/FormValidarFacultades.js"></script>
   <script src="js/FormValidarCiclos.js"></script>
-  <script src="js/ObtnerIDEmpresa.js"></script>
+  <script  >//No se si se usa pero para que no marque el error lo quito src="js/ObtnerIDEmpresa.js"</script>
   <script src="js/FormValidarReunion.js"></script>
   <Script src="js/alerta.js"></Script>
   <script src="js/SubidaArchivo.js"></script>
-  
+
 
 
   <!-- Menu Toggle Script -->
@@ -63,15 +46,15 @@
   </script>
 
   <script>
-    
+
     $(function() {
-    $("#btnExito").click(function(){        
+    $("#btnExito").click(function(){
       $('#modal_exito').modal('show');
     });
 });
 
 $(function() {
-    $("#btnFalla").click(function(){        
+    $("#btnFalla").click(function(){
       $('#modal_falla').modal('show');
     });
 });
@@ -79,110 +62,91 @@ $(function() {
 
 
   <script>
-  
+
 $(document).ready(function() {
-    // Setup - add a text input to each footer cell
-    $('#example thead tr').clone(true).appendTo( '#example thead' );
-    $('#example thead tr:eq(1) th').each( function (i) {
-        var title = $(this).text();
-        if (title.trim() == 'Actualizar') {//Se oculta el input de la parte de graficos ya que no se usara
-            $(this).html(
-                '<div >' +
-                '<input  class="form-control" style= "display: none;" type="text" placeholder="' + title + ' ">' +
-                '</div>');
-        }
-        else if (title.trim() == 'Ver expediente')
-        {
-           $(this).html(
-                '<div >' +
-                '<input  class="form-control" style= "display: none;" type="text" placeholder="' + title + ' ">' +
-                '</div>');
-         }
-         else if (title.trim() == 'Todos')
-        {
-           $(this).html(
-                '<div >' +
-                '<input  class="form-control" style= "display: none;" type="text" placeholder="' + title + ' ">' +
-                '</div>');
-         }
-          else if (title.trim() == 'Notas')
-        {
-           $(this).html(
-                '<div >' +
-                '<input  class="form-control" style= "display: none;" type="text" placeholder="' + title + ' ">' +
-                '</div>');
-         }
-          else if (title.trim() == 'Detalles')
-        {
-           $(this).html(
-                '<div >' +
-                '<input  class="form-control" style= "display: none;" type="text" placeholder="' + title + ' ">' +
-                '</div>');
-         }
+  var table = $('#example').DataTable({
 
-         else {
-            $(this).html(
-                '<div >' +
-                '<input  type="text" placeholder="' + title + ' ">' +
-                '</div>');
-        }
- 
-        $( 'input', this ).on( 'keyup change', function () {
-            if ( table.column(i).search() !== this.value ) {
-                table
-                    .column(i)
-                    .search( this.value )
-                    .draw();
-            }
-        } );
-    } );
+        "scrollX": true,
+        "scrollY": "50vh",
+        //Esto sirve que se auto ajuste la tabla al aplicar un filtro
+         "scrollCollapse": true,
 
-
-
-    var table = $('#example').DataTable( {
-        orderCellsTop: true,
-        fixedHeader: true,
-
-               
         language: {
-        "decimal": "",
-        "emptyTable": "No hay información",
-        "info": "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
-        "infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
-        "infoFiltered": "(Filtrado de _MAX_ total entradas)",
-        "infoPostFix": "",
-        "thousands": ",",
-        "lengthMenu": "Mostrar _MENU_ Entradas",
-        "loadingRecords": "Cargando...",
-        "processing": "Procesando...",
-        "search": "Buscar:",
-        "zeroRecords": "Sin resultados encontrados",
-        "paginate": {
-            "first": "Primero",
-            "last": "Ultimo",
-            "next": "Siguiente",
-            "previous": "Anterior"
-        }
-    }
+            "decimal": "",
+            "emptyTable": "No hay información",
+            "info": "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
+            "infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
+            "infoFiltered": "(Filtrado de _MAX_ total entradas)",
+            "infoPostFix": "",
+            "thousands": ",",
+            "lengthMenu": "Mostrar _MENU_ Entradas",
+            "loadingRecords": "Cargando...",
+            "processing": "Procesando...",
+            "search": "Buscar:",
+            "zeroRecords": "Sin resultados encontrados",
+            "paginate": {
+                "first": "Primero",
+                "last": "Ultimo",
+                "next": "Siguiente",
+                "previous": "Anterior"
+            }
+        },
 
+        initComplete: function() {
+            //En el columns especificamos las columnas que queremos que tengan filtro
+            this.api().columns([0,1,2,3,4,5,6,7,8]).every(function() {
+                var column = this;
+
+                var select = $('<select><option value=""></option></select>')
+                    .appendTo($(column.header()))
+                    .on('change', function() {
+                        var val = $.fn.dataTable.util.escapeRegex(
+                            $(this).val().trim()
+                        );
+
+                            column
+                            .search(val ? '^' + val + '$' : '', true, false)
+                            .draw();
+
+
+                    });
+                    //Este codigo sirve para que no se active el ordenamiento junto con el filtro
+                $(select).click(function(e) {
+                    e.stopPropagation();
+                });
+                //===================
+
+                column.data().unique().sort().each(function(d, j) {
+                    // select.append('<option value="' + d + '">' + d + '</option>')
+
+                        select.append('<option value="' + d + '">' + d + '</option>')
+
+                });
+
+
+
+            });
+        },
+        "aoColumnDefs": [
+         { "bSearchable": false
+         //"aTargets": [ 1] sirve para indicar que columna no queremos que funcione el filtro
+
+          }
+       ]
 
     });
-
-
-
-
+    //********Esta bendita linea hace la magia, adjusta el header de la tabla con el body
+    table.columns.adjust();
 } );
 
 </script>
 
 
 <script>
-  
-
     var table2 = $('#tableUser').DataTable({
         orderCellsTop: true,
         fixedHeader: true,
-        
+
         language: {
         "decimal": "",
         "emptyTable": "No hay información",
@@ -208,12 +172,10 @@ $(document).ready(function() {
 </script>
 
 <script>
-  
-
     var table2 = $('#tableUser2').DataTable({
         orderCellsTop: true,
         fixedHeader: true,
-        
+
         language: {
         "decimal": "",
         "emptyTable": "No hay información",
@@ -237,8 +199,34 @@ $(document).ready(function() {
     });
 
 </script>
-
-
+[2:10 p. m., 24/7/2020] Carlost: <script>
+$(document).ready(function(){
+var current = 1,current_step,next_step,steps;
+steps = $("fieldset").length;
+$(".next").click(function(){
+current_step = $(this).parent();
+next_step = $(this).parent().next();
+next_step.show();
+current_step.hide();
+setProgressBar(++current);
+});
+$(".previous").click(function(){
+current_step = $(this).parent();
+next_step = $(this).parent().prev();
+next_step.show();
+current_step.hide();
+setProgressBar(--current);
+});
+setProgressBar(current);
+// Change progress bar action
+function setProgressBar(curStep){
+var percent = parseFloat(100 / steps) * curStep;
+percent = percent.toFixed();
+$(".progress-bar")
+.css("width",percent+"%")
+.html(percent+"%");
+}
+});</script>
 </body>
 
 </html>
--- SuperUsuario/Modularidad/PiePagina.php
+++ SuperUsuario/Modularidad/PiePagina.php
@@ -1,4 +1,4 @@
-</div>          
+</div>
 </div>
 </div>
 <!-- FOOTER START -->
@@ -9,17 +9,17 @@
           <span style="margin-right:50px; margin-left:50px; color:white; font-size: 18px;">© 2020 Copyright: Pograma Oportunidades</span>
           <span style="color: white; font-weight: bold; font-size: 18px;">Contáctanos:</span><a href="https://www.facebook.com/exalumnos.ccgk"><img class="img-fluid" src="../img/facebook.png" style="margin-left:30px; width:60px;"></img></a>
           <a href="https://instagram.com/bk2oportunidades?igshid=4rmcd55eld5h"><img class="img-fluid" src="../img/instagram.png" style="margin-left:30px; width:60px;"></a></img>
-          
+
   </div>
 
 <!-- END OF FOOTER -->
 
   <!-- Bootstrap core JavaScript -->
-  
+
   <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
   <script src="js/alerta.js"></script> <!--Mostrar el mensaje de alerta-->
 
- 
+
 
 
 
@@ -30,11 +30,11 @@
   <script src="js/FormCarreras.js" ></script>
   <script src="js/FormValidarFacultades.js"></script>
   <script src="js/FormValidarCiclos.js"></script>
-  <script src="js/ObtnerIDEmpresa.js"></script>
+  <script  >//No se si se usa pero para que no marque el error lo quito src="js/ObtnerIDEmpresa.js"</script>
   <script src="js/FormValidarReunion.js"></script>
   <Script src="js/alerta.js"></Script>
   <script src="js/SubidaArchivo.js"></script>
-  
+
 
 
   <!-- Menu Toggle Script -->
@@ -46,105 +46,31 @@
   </script>
 
   <script>
-    
+
     $(function() {
-    $("#btnExito").click(function(){        
+    $("#btnExito").click(function(){
       $('#modal_exito').modal('show');
     });
 });
 
 $(function() {
-    $("#btnFalla").click(function(){        
+    $("#btnFalla").click(function(){
       $('#modal_falla').modal('show');
     });
 });
   </script>
 
-<script>
-$(document).ready(function() {
-    var table = $('#example2').DataTable({
-        
-        "scrollX": true,
-        "scrollY": "50vh",
-        //Esto sirve que se auto ajuste la tabla al aplicar un filtro
-         "scrollCollapse": true,
-     
-        language: {
-            "decimal": "",
-            "emptyTable": "No hay información",
-            "info": "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
-            "infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
-            "infoFiltered": "(Filtrado de _MAX_ total entradas)",
-            "infoPostFix": "",
-            "thousands": ",",
-            "lengthMenu": "Mostrar _MENU_ Entradas",
-            "loadingRecords": "Cargando...",
-            "processing": "Procesando...",
-            "search": "Buscar:",
-            "zeroRecords": "Sin resultados encontrados",
-            "paginate": {
-                "first": "Primero",
-                "last": "Ultimo",
-                "next": "Siguiente",
-                "previous": "Anterior"
-            }
-        },
-        
-        initComplete: function() {
-            //En el columns especificamos las columnas que queremos que tengan filtro
-            this.api().columns([0,1,2,3,4]).every(function() {
-                var column = this;
 
-                var select = $('<select><option value=""></option></select>')
-                    .appendTo($(column.header()))
-                    .on('change', function() {
-                        var val = $.fn.dataTable.util.escapeRegex(
-                            $(this).val()
-                        );
-                         
-                            column
-                            .search(val ? '^' + val + '$' : '', true, false)
-                            .draw();
-                        
-                        
-                    });
-                    //Este codigo sirve para que no se active el ordenamiento junto con el filtro
-                $(select).click(function(e) {
-                    e.stopPropagation();
-                });
-                //===================
-
-                column.data().unique().sort().each(function(d, j) {
-                    // select.append('<option value="' + d + '">' + d + '</option>')
-                   
-                        select.append('<option value="' + d + '">' + d + '</option>')
-                    
-                });
-
-                
-
-            });
-        },
-        "aoColumnDefs": [
-         { "bSearchable": false, "aTargets": [ 1 ] }
-       ] 
-      
-    });
-    //********Esta bendita linea hace la magia, adjusta el header de la tabla con el body 
-    table.columns.adjust();
-
-});
-</script>
   <script>
-  
+
 $(document).ready(function() {
   var table = $('#example').DataTable({
-        
+
         "scrollX": true,
         "scrollY": "50vh",
         //Esto sirve que se auto ajuste la tabla al aplicar un filtro
          "scrollCollapse": true,
-     
+
         language: {
             "decimal": "",
             "emptyTable": "No hay información",
@@ -165,7 +91,7 @@ $(document).ready(function() {
                 "previous": "Anterior"
             }
         },
-        
+
         initComplete: function() {
             //En el columns especificamos las columnas que queremos que tengan filtro
             this.api().columns([0,1,2,3,4,5,6,7,8]).every(function() {
@@ -175,14 +101,14 @@ $(document).ready(function() {
                     .appendTo($(column.header()))
                     .on('change', function() {
                         var val = $.fn.dataTable.util.escapeRegex(
-                            $(this).val()
+                            $(this).val().trim()
                         );
-                         
+
                             column
                             .search(val ? '^' + val + '$' : '', true, false)
                             .draw();
-                        
-                        
+
+
                     });
                     //Este codigo sirve para que no se active el ordenamiento junto con el filtro
                 $(select).click(function(e) {
@@ -192,153 +118,35 @@ $(document).ready(function() {
 
                 column.data().unique().sort().each(function(d, j) {
                     // select.append('<option value="' + d + '">' + d + '</option>')
-                   
+
                         select.append('<option value="' + d + '">' + d + '</option>')
-                    
+
                 });
 
-                
+
 
             });
         },
         "aoColumnDefs": [
-         { "bSearchable": false, "aTargets": [ 1 ] }
-       ] 
-      
+         { "bSearchable": false
+         //"aTargets": [ 1] sirve para indicar que columna no queremos que funcione el filtro
+
+          }
+       ]
+
     });
-    //********Esta bendita linea hace la magia, adjusta el header de la tabla con el body 
+    //********Esta bendita linea hace la magia, adjusta el header de la tabla con el body
     table.columns.adjust();
-
-
-
-  //La de Marquez
-    // Setup - add a text input to each footer cell
-  /*  $('#example thead tr').clone(true).appendTo( '#example thead' );
-    $('#example thead tr:eq(1) th').each( function (i) {
-        var title = $(this).text();
-        if (title.trim() == 'Actualizar') {//Se oculta el input de la parte de graficos ya que no se usara
-            $(this).html(
-                '<div >' +
-                '<input  class="form-control" style= "display: none;" type="text" placeholder="' + title + ' ">' +
-                '</div>');
-        }
-        else if (title.trim() == 'Ver expediente')
-        {
-           $(this).html(
-                '<div >' +
-                '<input  class="form-control" style= "display: none;" type="text" placeholder="' + title + ' ">' +
-                '</div>');
-         }
-         else if (title.trim() == 'Todos')
-        {
-           $(this).html(
-                '<div >' +
-                '<input  class="form-control" style= "display: none;" type="text" placeholder="' + title + ' ">' +
-                '</div>');
-         }
-          else if (title.trim() == 'Notas')
-        {
-           $(this).html(
-                '<div >' +
-                '<input  class="form-control" style= "display: none;" type="text" placeholder="' + title + ' ">' +
-                '</div>');
-         }
-          else if (title.trim() == 'Detalles')
-        {
-           $(this).html(
-                '<div >' +
-                '<input  class="form-control" style= "display: none;" type="text" placeholder="' + title + ' ">' +
-                '</div>');
-         }
-
-             else if (title.trim() == 'Lugar de asistencia')
-        {
-           $(this).html(
-                '<div >' +
-                '<input  class="form-control" style= "display: none;" type="text" placeholder="' + title + ' ">' +
-                '</div>');
-         }
-                else if (title.trim() == 'Asistencia')
-        {
-           $(this).html(
-                '<div >' +
-                '<input  class="form-control" style= "display: none;" type="text" placeholder="' + title + ' ">' +
-                '</div>');
-         }
-
-               else if (title.trim() == 'Cargo')
-        {
-           $(this).html(
-                '<div >' +
-                '<input  class="form-control" style= "display: none;" type="text" placeholder="' + title + ' ">' +
-                '</div>');
-         }
-
-         else {
-            $(this).html(
-                '<div >' +
-                '<input  type="text" placeholder="' + title + ' ">' +
-                '</div>');
-        }
- 
-        $( 'input', this ).on( 'keyup change', function () {
-            if ( table.column(i).search() !== this.value ) {
-                table
-                    .column(i)
-                    .search( this.value )
-                    .draw();
-            }
-        } );
-    } );
-
-
-
-    var table = $('#example').DataTable( {
-        orderCellsTop: true,
-        fixedHeader: true,
-
-               
-        language: {
-        "decimal": "",
-        "emptyTable": "No hay información",
-        "info": "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
-        "infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
-        "infoFiltered": "(Filtrado de _MAX_ total entradas)",
-        "infoPostFix": "",
-        "thousands": ",",
-        "lengthMenu": "Mostrar _MENU_ Entradas",
-        "loadingRecords": "Cargando...",
-        "processing": "Procesando...",
-        "search": "Buscar:",
-        "zeroRecords": "Sin resultados encontrados",
-        "paginate": {
-            "first": "Primero",
-            "last": "Ultimo",
-            "next": "Siguiente",
-            "previous": "Anterior"
-        }
-    }
-
-
-    });
-
-
-
-
-*/
-
-  } );
+} );
 
 </script>
 
 
 <script>
-  
-
     var table2 = $('#tableUser').DataTable({
         orderCellsTop: true,
         fixedHeader: true,
-        
+
         language: {
         "decimal": "",
         "emptyTable": "No hay información",
@@ -364,12 +172,10 @@ $(document).ready(function() {
 </script>
 
 <script>
-  
-
     var table2 = $('#tableUser2').DataTable({
         orderCellsTop: true,
         fixedHeader: true,
-        
+
         language: {
         "decimal": "",
         "emptyTable": "No hay información",
@@ -393,8 +199,34 @@ $(document).ready(function() {
     });
 
 </script>
-
-
+[2:10 p. m., 24/7/2020] Carlost: <script>
+$(document).ready(function(){
+var current = 1,current_step,next_step,steps;
+steps = $("fieldset").length;
+$(".next").click(function(){
+current_step = $(this).parent();
+next_step = $(this).parent().next();
+next_step.show();
+current_step.hide();
+setProgressBar(++current);
+});
+$(".previous").click(function(){
+current_step = $(this).parent();
+next_step = $(this).parent().prev();
+next_step.show();
+current_step.hide();
+setProgressBar(--current);
+});
+setProgressBar(current);
+// Change progress bar action
+function setProgressBar(curStep){
+var percent = parseFloat(100 / steps) * curStep;
+percent = percent.toFixed();
+$(".progress-bar")
+.css("width",percent+"%")
+.html(percent+"%");
+}
+});</script>
 </body>
 
 </html>
