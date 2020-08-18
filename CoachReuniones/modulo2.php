021d8a89c7e71db74b0ea05eca923b35dfc1206c
bryanherrera123 <bryan.herrera@oportunidades.org.sv>
2020-08-18 15:09:23 -0600

    Boton Aprobar / Reprobar Todos Reuniones

--- CoachReuniones/modulo2.php
+++ CoachReuniones/modulo2.php
@@ -82,10 +82,15 @@ include 'Modularidad/MenuVertical.php';
 	     		</a>
 		</span>
   </h5>	<br class="salto">
-<br class="salto">
-        <!-- <input type="submit" name="Aprobado" value="Aprobado" class="btn btn-primary btn-sm">
-        <input type="submit" name="Reprobado" value="Reprobado" class="btn btn-primary btn-sm"> -->
-    <br>
+  <br class="salto">
+  <br class="salto">
+  <br class="salto">
+  <br class="salto">
+  <br class="salto">
+  <div>
+  <input type="submit" name="Aprobado" value="Aprobado" class="btn btn-primary " style="background-color:#BE0032;">
+        <input type="submit" name="Reprobado" value="Reprobado" class="btn btn-primary " style="background-color:#BE0032;">
+      </div>
       <table  id="example" class="table table-sm table-bordered  h-100 w-100  "  >
       <br>
           <thead class="table-dark h-100 w-100">
