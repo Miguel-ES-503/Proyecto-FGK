be3ff0d1b8da271126a689c97a5084fba650678c
carlossalazar7 <carlossalazar2228@gmail.com>
2020-09-10 23:10:09 -0600

    cambios en alumnos /pensum

--- Alumno/Modelo/ModeloMaterias/eliminarInscripcion.php
+++ Alumno/Modelo/ModeloMaterias/eliminarInscripcion.php
@@ -1,38 +1,19 @@
- 
 <?php
 require_once "../../../BaseDatos/conexion.php";
-
-$iddescripcion = $_POST['idmateria'];
-
- $sql = $pdo->prepare("DELETE FROM `inscripcionmateria` WHERE `Id_InscripcidMateriaionM` =? ");
  
- if($sql->execute(array($iddescripcion))){
+echo $iddescripcion = $_POST['idmateria'];
 
-    echo "vaMOS bIEN";
+$sql = "DELETE FROM `inscripcionmateria` WHERE idMateria = '$iddescripcion' ";        
+$q = $pdo->prepare($sql);
 
-    $sql2 = $pdo->prepare("UPDATE FROM materias SET estadoM = null WHERE idMateria = ? ");
-    if($sql2->execute(array($iddescripcion))){
-        echo "Chale a funcionado";
-    }else{
-        echo "Chale no funciona";
-    }
-    
- }
-else{
-    echo "nEL";
-    echo $iddescripcion;
-}
+$response = $q->execute();
 
 
-
-
-
-
-
-
-
-
-
-
-
+  $sql2 = $pdo->prepare("UPDATE  materias SET estadoM = ? WHERE idMateria = ? ");
+  if($sql2->execute(array(" ",$iddescripcion))){
+      echo "Chale ya funcionado";
+      header("Location: ../../InscripcionMateriasCiclo.php?id=".$idInscripcionCiclo);
+  }else{
+      echo "Chale no funciona";
+  }
 ?>
