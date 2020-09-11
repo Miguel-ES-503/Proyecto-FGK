3f43458db0e3dbf1ebd01c9c718a630eac4f403e
tejadakatherine19 <tejadakatherine844@gmail.com>
2020-09-10 18:08:06 -0600

    responsive3.2

--- SuperUsuario/cambiardatos.php
+++ SuperUsuario/cambiardatos.php
@@ -3,6 +3,88 @@
 include 'Modularidad/CabeceraInicio.php';
 ?>
 <title>Actualizar talleres/módulos</title>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+
+.topnav {
+  overflow: hidden;
+  background-color: #ADADB2;
+  max-width: 100%;
+}
+
+.topnav a {
+  float: left;
+  display: block;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  border-width: 3px;
+  font-weight: bold;
+
+ 
+}
+.submenu1{
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 18px;
+  background-color: #9d120e;
+  border-width: 3px;
+  font-weight: bold;
+  height: 68px;
+  letter-spacing: 2px;
+
+
+
+}
+.icon{
+  
+
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #4CAF50;
+  color: white;
+}
+
+.topnav .icon1 {
+  display: none;
+}
+
+@media screen and (max-width: 600px) {
+  .topnav a:not(:first-child) {display: none;}
+  .topnav a.icon1 {
+    float: right;
+    display: inline-block;
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .topnav.responsive {position: relative;}
+  .topnav.responsive .icon {
+    position: absolute;
+    right: 0;
+    top: 0;
+  }
+  .topnav.responsive a {
+    float: none;
+    display: block;
+    text-align: left;
+    font-size: 15px;
+    height: 50px;
+  }
+  .titulomenu a{
+    font-size: 15px;
+  }
+}
+</style>
 <?php
 
 //Modularaidad para extraere los enlaces en HEAD
@@ -18,20 +100,39 @@ require_once "../BaseDatos/conexion.php";
     display: none;
 }
 </style>
+<script>
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+</script>
+
 <link rel="stylesheet" type="text/css" href="css/modulos-moodle.css">
 
 <?php
 $id = $_POST['id'];
 $stmt2 = $pdo->query("SELECT * FROM alumnos WHERE ID_Alumno = '$id' ");
 ?>
-<div class="title">
-    <a href="javascript:history.back();" title=""><img src="../img/back.png" class="icon"></a>
-    <h2 class="main-title">Actualizar talleres/módulos</h2>
-    <div class="title2">
-    </div>
+
+<div class="topnav" id="myTopnav">
+  <a href="javascript:history.back();"><img src="../img/proximo.svg" class="icon"></a>
+  <a  class="titulomenu" style="background-color:#ADADB2; color: #2D2D2E; font-size: 25px;">Actualizar talleres/módulos</a>
+ 
+  
+  <a href="javascript:void(0);" class="icon1" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
 </div>
+
+
 <!--Comiezo de estructura de trabajo -->
 <div class="container-fluid text-center" id="main-inicio">
+
+    
     <div class="container w-50">
         <h1>Registrar cambios</h1>
         <div class="progress">
