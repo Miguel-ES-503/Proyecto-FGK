b9f6dd2360dd4dbb356ea2ece992f1472938f0a1
Jairo090101 <jairo.hernandez090101@gmail.com>
2020-10-13 17:45:30 -0600

    Conexion

--- BaseDatos/conexion.php
+++ BaseDatos/conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-$pdo = new PDO('mysql:host=localhost;dbname=oportuni_despega_desarrollo','oportuni_Admin','UDB123456@');
+$pdo = new PDO('mysql:host=localhost;dbname=oportuni_despega','root','');
 //$pdo = new PDO('mysql:host=fdb23.awardspace.net;dbname=3502401_miguel',"3502401_miguel","-ypmq9b5x");
 
 ?>
--- Conexion/conexion.php
+++ Conexion/conexion.php
@@ -1,8 +1,8 @@
 <?php
 try {
-    $dbname="oportuni_despega_desarrollo";
-    $user="oportuni_Admin";
-    $password="UDB123456@";
+    $dbname="oportuni_despega";
+    $user="root";
+    $password="";
     $dsn = "mysql:host=localhost;dbname=$dbname";
     $dbh = new PDO($dsn, $user, $password);
 //$dbh = new PDO('mysql:host=fdb23.awardspace.net;dbname=3502401_miguel',"3502401_miguel","-ypmq9b5x");
